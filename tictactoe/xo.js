ee70464f0837aa920d8bd6b134baa5fc4f717995
Michael Pogrebnjak <astroqueih@gmail.com>
2020-11-24 17:11:21 +0100

    0.1

---
+++ tictactoe/xo.js
@@ -0,0 +1,17 @@
+function x(row, col){
+
+} function o(row, col){
+
+}
+//startup
+var canvas = document.getElementById("cv");
+var ctx = canvas.getContext("2d");
+var coordinate = {
+    sizeFactor:800/3;
+    localX:function(){
+
+    },
+    localY:function(){
+        
+    }
+};
